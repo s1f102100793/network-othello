4635b2fb8d42f236f41dc217542078a1942a921b
s1f102100793 <s1f102100793@iniad.org>
2023-08-16 19:37:10 +0900

    /battleを作ってそこにothelloのやつを移動

--- client/src/pages/battle/index.page.tsx
+++ client/src/pages/battle/index.page.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
 import { apiClient } from 'src/utils/apiClient';
 import { returnNull } from 'src/utils/returnNull';
-import type { BoardArr } from '../../../server/useCase/boardUseCase';
-import styles from './battle/index.module.css';
+import type { BoardArr } from '../../../../server/useCase/boardUseCase';
+import styles from './index.module.css';
 
 const Home = () => {
   const [board, setBoard] = useState<BoardArr>([
