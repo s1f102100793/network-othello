f015276d7220d41d0ba0a4cd611d2af5cfbef072
s1f102100793 <s1f102100793@iniad.org>
2023-08-18 23:27:07 +0900

    サイドバー色々

--- client/src/components/sidebar.tsx
+++ client/src/components/sidebar.tsx
@@ -1,16 +1,28 @@
+import { useState } from 'react';
 import styles from '../styles/Sidebar.module.css';
 
 const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className={styles.sidebar}>
-      {/* サイドバーの内容をこちらに記述 */}
-      <h2>サイドバー</h2>
-      <ul>
-        <li>項目1</li>
-        <li>項目2</li>
-        <li>項目3</li>
-      </ul>
-    </div>
+    <>
+      <button 
+        className={styles.hamburger} 
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        ☰
+      </button>
+      {isOpen && (
+        <div className={styles.sidebar}>
+          <h2>サイドバー</h2>
+          <ul>
+            <li>項目1</li>
+            <li>項目2</li>
+            <li>項目3</li>
+          </ul>
+        </div>
+      )}
+    </>
   );
 };
 
